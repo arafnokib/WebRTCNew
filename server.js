2b37119efc141ae521036be2e71b665fede95634
Araf <arafnokib1@gmail.com>
2022-10-10 20:48:46 -0400

    email

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerServer);
+var nodemailer = require("nodemailer")
+const transporter = nodemailer.createTransport({
+    port: 587,
+    host: "smtp.gmail.com",
+    auth: {
+        user: "arafnokibtest@gmail.com",
+        pass: "goljqugvzasnvtje"
+    },
+    secure: true
+})
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -26,6 +36,26 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
+
+app.post("/send-mail", (req,res)=>{
+    const to = req.body.to
+    const url = req.body.url
+    const maildata = {
+        from: "arafnokibtest@gmail.com",
+        to: to,
+        subject: "Join this video chat with me!",
+        html: `<p>Hello</p> <p>Come and join me for a video chat at ${url}</p>`
+    }
+    transporter.sendMail(maildata, (error, info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({
+            message: "Invite sent.",
+            message_id: info.messageId
+        })
+    })
+})
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
